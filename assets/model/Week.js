0418efcb7b9c15651479a679939de3b3476636b4
Kup96 <philatov96@gmail.com>
2021-10-07 19:28:02 +0300

    redacts

--- assets/model/Week.js
+++ assets/model/Week.js
@@ -1,9 +1,12 @@
 'use strict';
-export default function weekDayFunction(){
+function weekDayFunction(){
         let days = ["Вc", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
         let weekDay = days[new Date().getDay()];
         return weekDay;
 }
-export default weekDayNumber = [new Date().getDay()]
+
+let weekDayNumber = new Date().getDay();
+
+export default {weekDayNumber, weekDayFunction}
 
 
